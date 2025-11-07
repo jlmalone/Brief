fe676a7b5ab8c4e801bf5a5387184c2fed047168
Joseph Malone <joseph.malone@walmart.com>
2025-11-07 17:29:42 -0500

    incremental

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,9 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.detekt) apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
